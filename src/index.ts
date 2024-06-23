c712c4398dcf475e23c43d6ecd8155387f02e2b5
fnaith <weichih.fr.lai@gmail.com>
2024-06-23 20:40:22 +0800

    wip

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 import {v4 as uuidv4} from 'uuid'
 const crypto = require('crypto');
 const mongoose = require('mongoose');
+const undici = require('undici');
+import { ProxyAgent } from 'undici'
 
 const algorithm = 'AES-256-CBC';
 const secrateKey = '11112222333344445555666677778888';
@@ -65,6 +67,19 @@ const fastify = require('fastify')()
 
 const createError = require('@fastify/error')
 
+// async function initRate() {
+//   await fastify.register(import('@fastify/rate-limit'), {
+//     global: true,
+//     max: 1,
+//     timeWindow: 600000,
+//     keyGenerator: function (request: any) {
+//       console.warn(request.ip)
+//       return request.ip
+//     }
+//   })
+// }
+// await initRate().catch(err => console.log(err));
+
 const { fastifySchedulePlugin } = require('@fastify/schedule');
 const { SimpleIntervalJob, AsyncTask } = require('toad-scheduler');
 
@@ -118,9 +133,30 @@ fastify.decorate("internalAuth", async function(request: any, reply: any) {
   }
 })
 
+const proxyAgent = new ProxyAgent('http://127.0.0.1:8080')
+
+fastify.get('/health', async (request: any, reply: any) => {
+  // const {
+  //   statusCode,
+  //   headers,
+  //   trailers,
+  //   body
+  // } = await undici.request('http://127.0.0.1:8080/health2')
+  // return reply.send(body)
+  const {
+    statusCode,
+    body
+  } = await undici.request('http://127.0.0.1:8080/health2'/*, { dispatcher: proxyAgent }*/)
+  return reply.code(statusCode).send(body)
+})
+
+fastify.get('/health2', async (request: any, reply: any) => {
+  return 'ok'
+})
+
 fastify.get('/ping', { onRequest: [fastify.jwtAuth, fastify.internalAuth] }, async (request: any, reply: any) => {
   console.info(request.user)
-  return 'pong\n'
+  return 'pong'
 })
 
 fastify.post('/signup', (request: any, reply: any) => {
@@ -147,6 +183,84 @@ fastify.post('/signup', (request: any, reply: any) => {
 //     return err
 //   }
 // })
+
+import OpenAI from "openai";
+var stream = require('stream')
+
+export const searchGPT = async (question: any) => {
+  const openai = new OpenAI();
+
+  const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: "user", content: question }],
+    model: "gpt-3.5-turbo",
+  });
+
+  return chatCompletion.choices[0].message.content;
+}
+
+process.env.OPENAI_API_KEY="sk-proj-2jCIx0qPkwYnwfzcih7aT3BlbkFJYxEloELVNs0FfnZ6Iokh"
+
+const search = async (request: any) => {
+  const { question } = request.query;
+
+  if (!question) {
+    return { message: "invalid" };
+  }
+
+  const message = await searchGPT(question);
+  return { message };
+}
+
+const gptRoutes = (fastify: any, reply: any, done: any) => {
+  fastify.get("/gpt", search);
+  done();
+};
+
+fastify.register(gptRoutes, { prefix: "v1" })
+
+fastify.get('/stream', function (request: any, reply: any) {
+
+	// Create a buffer to hold the response chunks
+	var buffer = new stream.Readable();
+	buffer._read = ()=>{};
+
+	// Generate 5 chunks with 1 second interval
+	var count = 5;
+	var emit = () => {
+		var data = `Hello World ${count}`
+		buffer.push(data);
+
+		count--;
+		if (count > 0) {
+			setTimeout(emit, 1000);
+		}
+		else {
+			console.log('end sending.');
+			buffer.push(null);
+		}
+	};
+
+	emit();
+	reply.type('text/html').send(buffer)
+})
+
+fastify.get('/stream/gpt', async function (request: any, reply: any) {
+  const openai = new OpenAI();
+  const { question } = request.query;
+  const streamCompletions = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: [{ role: 'user', content: question }],
+    stream: true,
+  });
+
+	const buffers = []
+  for await (const chunk of streamCompletions) {
+    var data = chunk.choices[0]?.delta?.content || ''
+    buffers.push(data)
+  }
+
+	reply.type('text/html').send(buffers.join(''))
+})
 
 fastify.listen({ port: 8080 }, (err: any, address: any) => {
   if (err) {
